5b5b1a25769785fe25f1f90295a5212f8921d2b3
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-10-14 23:58:29 -0300

    feat: create axiosSeniorGetaway

--- src/services/senior/axios-senior.ts
+++ src/services/senior/axios-senior.ts
@@ -3,3 +3,7 @@ import axios, { AxiosInstance } from 'axios'
 export const axiosSenior: AxiosInstance = axios.create({
   baseURL: 'https://platform.senior.com.br/'
 })
+
+export const axiosSeniorGetaway: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:3001/'
+})
