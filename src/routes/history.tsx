1e8135d9631499da79d50fefc31f49a088a4e1c1
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2023-01-05 23:13:26 -0300

    feat: create select from year

--- src/routes/history.tsx
+++ src/routes/history.tsx
@@ -1,30 +1,49 @@
-import { Select, VStack } from '@chakra-ui/react'
+import { HStack, Select, VStack } from '@chakra-ui/react'
 import { useState } from 'react'
 import { MonthEventTable } from '../components/MonthEventTable'
 import { useSeniorContext } from '../hooks/useSenior'
 
 export const History = () => {
-  const { monthlyReport } = useSeniorContext()
+  const { monthlyReport, year, setYear } = useSeniorContext()
   const [value, setValue] = useState(-1)
   const date = new Date()
+  const currentYear = date.getFullYear()
 
   return (
     <VStack width="full" marginX={{ base: '1rem', md: '5rem' }} paddingY="5rem">
-      <Select
-        placeholder="Escolha um mês"
-        variant="flushed"
-        value={value}
-        onChange={(event) => setValue(Number(event.target.value))}
-      >
-        {monthlyReport.map((_, index) => {
-          date.setMonth(index)
-          return (
-            <option key={index} value={index}>
-              {date.toLocaleString('pt-BR', { month: 'long' }).toUpperCase()}
-            </option>
-          )
-        })}
-      </Select>
+      <HStack width="full">
+        <Select
+          width="70%"
+          placeholder="Escolha um mês"
+          variant="flushed"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        >
+          {monthlyReport.map((_, index) => {
+            date.setMonth(index)
+            return (
+              <option key={index} value={index}>
+                {date.toLocaleString('pt-BR', { month: 'long' }).toUpperCase()}
+              </option>
+            )
+          })}
+        </Select>
+        <Select
+          width="30%"
+          variant="flushed"
+          value={year}
+          onChange={(event) => setYear(Number(event.target.value))}
+        >
+          {[currentYear, currentYear - 1].map((year) => {
+            return (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            )
+          })}
+        </Select>
+      </HStack>
+
       {value >= 0 && <MonthEventTable data={monthlyReport[value]} />}
     </VStack>
   )
